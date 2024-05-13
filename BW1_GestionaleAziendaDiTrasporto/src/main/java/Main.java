52226379a8028972d610439e07d17914e758cd87
stefortezza <stefano.fortezza98@hotmail.it>
2024-05-13 20:28:28 +0200

    ok

--- BW1_GestionaleAziendaDiTrasporto/src/main/java/Main.java
+++ BW1_GestionaleAziendaDiTrasporto/src/main/java/Main.java
@@ -8,10 +8,8 @@ import entity.ticket.rivenditore.RivenditoreAutorizzato;
 import entity.tratta.Tratta;
 import entity.tratta.Viaggio;
 import entity.utente.Tessera;
-import enums.Autorizzazione;
-import enums.TipoAbbonamento;
-import enums.TipoMezzo;
-import enums.TipoStatoMezzo;
+import entity.utente.Utente;
+import enums.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -25,7 +23,7 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("bw4");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("BW1_GestionaleAziendaDiTrasporto");
         EntityManager em = emf.createEntityManager();
 
         UtenteDAO utenteDAO = new UtenteDAO(em);
@@ -36,21 +34,21 @@ public class Main {
         MezzoDAO mezzoDAO = new MezzoDAO(em);
         TrattaDAO trattaDAO = new TrattaDAO(em);
         ViaggioDAO viaggioDAO = new ViaggioDAO(em);
+
+        Utente u1 = new Utente();
+        u1.setNome("Stefano");
+        u1.setCognome("Fortezza");
+        u1.setEmail("stefano.fortezza@epicode.it");
+        u1.setDataDiNascita(LocalDate.of(1998, 7, 28));
+        u1.setSesso(Sesso.UOMO);
+        u1.setNumeroTelefono("+3934085890692");
+
+        utenteDAO.save(u1);
 //
-//        Utente u1 = new Utente();
-//        u1.setNome("Stefano");
-//        u1.setCognome("Fortezza");
-//        u1.setEmail("stefano.fortezza@epicode.it");
-//        u1.setDataDiNascita(LocalDate.of(1998, 7, 28));
-//        u1.setSesso(Sesso.UOMO);
-//        u1.setNumeroTelefono("+3934085890692");
-//
-//        utenteDAO.save(u1);
-//
-//        Tessera t1 = new Tessera();
-//        t1.setUtente(u1);
-//        t1.setDataEmissione(LocalDate.now());
-//        tesseraDAO.save(t1);
+        Tessera t1 = new Tessera();
+        t1.setUtente(u1);
+        t1.setDataEmissione(LocalDate.now());
+        tesseraDAO.save(t1);
 
 //        DistributoreAutomatico da1 = new DistributoreAutomatico();
 //        da1.aggiungiDisponibilita(100);
